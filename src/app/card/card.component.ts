fa0cecf7e7257471b020c753fb06d06b422450be
joel baertschi <joel.ba@hotmail.com>
2024-10-29 22:25:19 +0100

    feat: added list of card and buttons to change blog

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,13 +1,19 @@
 // src/app/card/card.component.ts
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-card',
   standalone: true,
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.scss'],
-  imports: [MatCardModule, MatButtonModule], // Material Modules importieren
+  imports: [MatCardModule, MatButtonModule, CommonModule], // Material Modules importieren
 })
-export class CardComponent {}
+export class CardComponent {
+  @Input() title!: string; // Eingabe für den Titel der Karte
+  @Input() subtitle!: string; // Eingabe für den Untertitel der Karte
+  @Input() image!: string; // Eingabe für das Bild der Karte
+  @Input() content!: string;
+}
