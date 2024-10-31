2d9e8960ff5123c5135c7a016c90cf2d7018c8ab
joel baertschi <joel.ba@hotmail.com>
2024-10-31 22:14:29 +0100

    test: fixed tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,17 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { BlogService } from './service/blog.service';
 
 describe('AppComponent', () => {
+  let blogServiceMock: jasmine.SpyObj<BlogService>;
+
   beforeEach(async () => {
+    // Create a mock BlogService with the `getBlogs` method
+    blogServiceMock = jasmine.createSpyObj('BlogService', ['getBlogs']);
+
     await TestBed.configureTestingModule({
       imports: [AppComponent],
+      providers: [{ provide: BlogService, useValue: blogServiceMock }],
     }).compileComponents();
   });
 
@@ -14,16 +21,9 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'blog-app' title`, () => {
+  it(`should have the title 'blog-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('blog-app');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('blog-app');
   });
 });
--- src/app/service/blog.service.spec.ts
+++ src/app/service/blog.service.spec.ts
@@ -1,13 +1,25 @@
 import { TestBed } from '@angular/core/testing';
-
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 import { BlogService } from './blog.service';
 
 describe('BlogService', () => {
   let service: BlogService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [BlogService],
+    });
     service = TestBed.inject(BlogService);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify(); // Verifiziert, dass keine weiteren offenen Anfragen bestehen
   });
 
   it('should be created', () => {
